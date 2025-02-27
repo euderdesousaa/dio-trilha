cdf49eb4e2aaca2a9c177541f751eb23165c1f25
Euder de Sousa <euderdesousaaaa@gmail.com>
2025-02-27 12:26:28 -0300

    Design Patterns desafio

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.gof.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+}
